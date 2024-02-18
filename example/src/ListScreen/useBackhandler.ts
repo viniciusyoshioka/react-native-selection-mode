dab9ff75c3688c9432c0da407bc74415ea18abf7
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-02-17 21:50:16 -0300

    Create useBackhandler hook

---
+++ example/src/ListScreen/useBackhandler.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+import { BackHandler } from "react-native"
+
+
+export type BackhandlerCallback = () => boolean | null | undefined
+
+
+export function useBackhandler(callback: BackhandlerCallback) {
+    useEffect(() => {
+        const subscription = BackHandler.addEventListener("hardwareBackPress", callback)
+        return () => subscription.remove()
+    }, [callback])
+}
