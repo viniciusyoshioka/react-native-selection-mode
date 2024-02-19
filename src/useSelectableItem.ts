bef788abdfbdec8c547551b1646ac7c2bd701d13
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-02-19 13:58:00 -0300

    Add documentation for SelectableItem

--- src/useSelectableItem.ts
+++ src/useSelectableItem.ts
@@ -1,10 +1,41 @@
 
 
 export interface SelectableItem {
+
+    /**
+     * Function called when the item is clicked and the component is not
+     * in selection mode. Similar to `onPress`.
+     */
     onClick: () => void
+
+    /**
+     * When in selection mode, this function is called when the item is
+     * not selected and is pressed.
+     *
+     * If you want to select the item, you still have to call
+     * `useSelectionMode().select()` manually. If needed, its also possible
+     * to execute other actions.
+     */
     onSelect: () => void
+
+    /**
+     * When in selection mode, this function is called when the item is
+     * is selected and is pressed.
+     *
+     * If you want to deselect the item, you still have to call
+     * `useSelectionMode().deselect()` manually. If needed, its also possible
+     * to execute other actions.
+     */
     onDeselect: () => void
+
+    /**
+     * Indicates the selection mode state to the component.
+     */
     isSelectionMode: boolean
+
+    /**
+     * Indicates if the component is selected.
+     */
     isSelected: boolean
 }
 
