1900240d087df57e1a5e2629cc7f6adc7dcce4b8
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-22 15:11:30 -0300

    chore: Create tsconfig.eslint.json file for eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   root: true,
   extends: "@vinicius1313/eslint-config",
   parserOptions: {
+    project: "tsconfig.eslint.json",
     tsconfigRootDir: __dirname,
   },
   ignorePatterns: [
